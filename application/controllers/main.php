5beae6a0627f677b27b1fe2ed7836d44a0cbdc52
kalanaperera <kalana.p@findmyfare.com>
2015-09-18 23:19:24 +0530

    done
    done

---
+++ application/controllers/main.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * kalana
+ */
+
+class main extends CI_CONTROLLER{
+    
+    public function index(){
+       
+        $this->load->view('template/header');
+        $this->load->view('main');
+        $this->load->view('template/footer');
+        
+        
+        
+    }
+    
+    
+    
+}
---
+++ application/views/template/footer.php
@@ -0,0 +1,13 @@
+    <!-- Footer -->
+    <div class="footer">
+        Social Network Template &copy; Copyright Notice
+    </div>
+
+    <!-- // Footer -->
+    
+<!-- Vendor Scripts Bundle --><script src="/kathanayake/dist/js/vendor.min.js"></script>
+
+    <!-- App Scripts Bundle -->
+    <script src="/kathanayake/dist/js/scripts.min.js"></script>
+</body>
+</html>
