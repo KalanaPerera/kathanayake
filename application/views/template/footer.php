450456d3412fd4d739385650a3350cf5ca2ac8af
Kalana Perera ( Kalaa ) <kalana.p@findmyfare.com>
2015-09-19 23:12:43 +0530

    Update footer.php

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,11 +1,5 @@
-  <!-- Footer -->
-    <div class="footer">
-        Social Network Template &copy; Copyright Notice
-    </div>
-
-    <!-- // Footer -->
-<<<<<<< HEAD
-
+   
+<!-- Vendor Scripts Bundle --><script src="/kathanayake/dist/js/vendor.min.js"></script>
 
     <!-- App Scripts Bundle -->
     <script src="/kathanayake/dist/js/scripts.min.js"></script>
@@ -39,18 +33,26 @@ var markers1 = [];
 var infowindow = new google.maps.InfoWindow({
     content: ''
 });
-=======
->>>>>>> origin/master
-
+//health
+//education 
+//agriculture
+// Our markers
+markers1 = [
+    ['0', '   <div class="panel panel-default"><div class="panel-heading" style="background:  #869694 !important"><div class="media"><a href="" class="pull-left"><img src="/kathanayake/dist/images/people/50/guy-2.jpg" class="media-object"></a><div class="media-body"><div class="rating text-left pull-right"><span>   <i  style="cursor: pointer" class="fa fa-thumbs-up"></i> <span style="font-size: 12px;"> 20 </span> </span><span>  <i style="cursor: pointer" class="fa fa-thumbs-down"></i><span style="font-size: 12px;"> 7 </span> </span><!--                                <span class="star"></span><span class="star "></span><span class="star "></span><span class="star "></span><span class="star "></span>--></div><a href="">Jonathan</a><span>on 15th January, 2014</span></div></div></div><div class="panel-body"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad blanditiis perspiciatis praesentium quaerat repudiandae soluta? Cum doloribus esse et eum facilis impedit officiis omnis optio, placeat, quia quo reprehenderit sunt velit? Asperiores cumque deserunt eveniet hic reprehenderit sit, ut voluptatum?</p></div><div class="view-all-comments"><a href="#"><i class="fa fa-comments-o"></i> View all</a> 10 comments</div><ul class="comments"><li><div class="media"><a href="" class="pull-left"><img src="/kathanayake/dist/images/people/50/woman-5.jpg" class="media-object"></a><div class="media-body"><a href="" class="comment-author">Mary</a><span>Thanks Bill</span><div class="comment-date">2 days</div></div></div></li><li><div class="media"><a href="" class="pull-left"><img src="/kathanayake/dist/images/people/50/guy-5.jpg" class="media-object"></a><div class="media-body"><a href="" class="comment-author">Bill D.</a><span>What time did it finish?</span><div class="comment-date">14 min</div></div></div></li><li class="comment-form"><div class="input-group"><input type="text" class="form-control" /><span class="input-group-addon"><a href=""><i class="fa fa-photo"></i></a></span></div></li></ul></div>', 6.952204, 79.922993, 'health'],
+    ['1', 'Title', 7.087428, 80.014373, 'health'],
     
+    ['2', 'Title', 6.838686, 79.965009, 'education'],
+    ['3', 'Title', 6.905897, 79.969466, 'education'],
+    ['4', 'Title', 6.952204, 79.922993, 'education'],
+    ['5', 'Title', 5.954852, 80.553409, 'education'],
     
-<<<<<<< HEAD
+    ['6', 'Title', 8.345512, 80.393069, 'agriculture'],
+    ['7', 'Title', 6.972908, 79.915998, 'agriculture'],
+    
 ];
-
 /**
  * Function to init map
  */
-
 function initialize() {
     var center = new google.maps.LatLng( 6.950480, 80.004683);
     var mapOptions = {
@@ -58,32 +60,26 @@ function initialize() {
         center: center,
         mapTypeId: google.maps.MapTypeId.TERRAIN
     };
-
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     for (i = 0; i < markers1.length; i++) {
         addMarker(markers1[i]);
     }
 }
-
 /**
  * Function to add marker to map
  */
-
 function addMarker(marker) {
     var category = marker[4];
     var title = marker[1];
     var pos = new google.maps.LatLng(marker[2], marker[3]);
     var content = marker[1];
-
     marker1 = new google.maps.Marker({
         title: title,
         position: pos,
         category: category,
         map: map
     });
-
     gmarkers1.push(marker1);
-
     // Marker click listener
     google.maps.event.addListener(marker1, 'click', (function (marker1, content) {
         return function () {
@@ -95,11 +91,9 @@ function addMarker(marker) {
         }
     })(marker1, content));
 }
-
 /**
  * Function to filter markers by category
  */
-
 filterMarkers = function (category) {
     for (i = 0; i < markers1.length; i++) {
         marker = gmarkers1[i];
@@ -113,18 +107,12 @@ filterMarkers = function (category) {
         }
     }
 }
-
 // Init map
 initialize();
-
    
              
          })
          
      </script>
-
-=======
-  
->>>>>>> origin/master
 </body>
 </html>
